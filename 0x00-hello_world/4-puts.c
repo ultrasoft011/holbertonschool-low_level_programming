632383485989a0df2216dedf34957a52e02fcb08
ultrasoft011 <ultrasoft.011@gmail.com>
2019-09-18 21:12:44 +0000

    0x00 Hello World!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 # include <stdio.h>
-
 /**
  * Hello, puts
  *
- * Return: Always 0 
+ * Return: Always0
  */
-
-int main()
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
