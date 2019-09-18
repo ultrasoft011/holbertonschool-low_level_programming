57b0e076cf4370de46294268fdd21e3d37220378
ultrasoft011 <ultrasoft.011@gmail.com>
2019-09-18 22:01:01 +0000

    0x00 Hello World!

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+int main()
+{
+  std::cout << "Size of a char:\t\t" << sizeof(char) << " bytes\n";
+  std::cout << "Size of an int:\t\t" << sizeof(int) << " bytes\n";
+  std::cout << "Size of a long int:\t\t" << sizeof(long int) << " bytes\n";
+  std::cout << "Size long long int:\t" << sizeof(long long int) << " bytes
+    std::cout << "Size of a float:\t\t" << sizeof(float) << " bytes\n";
+    return 0;
+}
