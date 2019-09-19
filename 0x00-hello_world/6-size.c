0d1cfbc189049d70546ed182c61c4c06af3fa997
ultrasoft011 <55111526+ultrasoft011@users.noreply.github.com>
2019-09-18 20:38:38 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char\n", sizeof(char));
-printf("Size of an int\n", sizeof(int));
-printf("Size of a long int\n", sizeof(long int));
-printf("Size of long long int\n", sizeof(long long int));
-printf("Size of a float\n", sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
