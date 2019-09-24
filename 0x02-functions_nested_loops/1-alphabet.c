c2d7004e472fb444ed34cf7ac5b47c60354ebf5c
ultrasoft011 <ultrasoft.011@gmail.com>
2019-09-24 15:51:52 +0000

    Commands

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet(void) - prints the alphabet
  *
- * Return: Always 0.
+ * Return: null
  */
 void print_alphabet(void)
 {
