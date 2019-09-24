1ce2add1b479ee35e6c312d80068821ae3dce4ea
ultrasoft011 <ultrasoft.011@gmail.com>
2019-09-24 14:13:03 +0000

    Functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alx;
+for (alx = 'a'; alx <= 'z'; alx++)
+{
+_putchar(alx);
+}
+_putchar('\n');
+return (0);
+}
