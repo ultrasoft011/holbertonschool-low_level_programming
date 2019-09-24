2b5d37de7e83aada2d4a4e80735d0d771599e479
ultrasoft011 <ultrasoft.011@gmail.com>
2019-09-24 20:20:26 +0000

    Commands

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_isalpha - recognize letters and numbers
+ *@c: First operand
+ *Return: 1 if c is a letter 0 if is a number
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+putchar('1');
+}
+else
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+return (0);
+}
