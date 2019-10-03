99d24b48ffd1517f48ec2a9bc0888c7f8b6237b7
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-03 15:41:01 +0000

    Pointers 09

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *print_array - prints an array
+ *@a: Pointer
+ *@n: Variable
+ */
+
+void print_array(int *a, int n)
+{
+int counter = 0;
+
+for (counter = 0; counter < n; counter++)
+{
+if (counter == (n - 1) )  
+{
+printf("%d", a[counter]);
+}
+else 
+{
+printf("%d, ", a[counter]);
+}
+}
+printf("\n");
+}
