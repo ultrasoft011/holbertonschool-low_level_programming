dd019b27fa3721d8457beee9afa9cefce887c6ff
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-04 19:55:30 +0000

    More07

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * *string_toupper - Lowercase to Uppercase
+ *@s: Pointer
+ *Return: s
+ */
+char *string_toupper(char *s)
+{
+int i;
+
+while (s[i] != 0)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+{
+s[i] = s[i] - 32;
+}
+i++;
+}
+return (s);
+}
