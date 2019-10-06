0f7f957d7a7f78b48c538706cf1bc1391d38588b
ultrasoft011 <55111526+ultrasoft011@users.noreply.github.com>
2019-10-06 16:13:42 -0500

    Create 8-rot13.c

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* *rot13 - a string to rot13 codification
+*@a: Pointer variable
+*Return: a
+*/
+char *rot13(char *a);
+{
+int i = 0;
+int j = 0;
+char testarr1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char testarr2[] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+for (i = 0; a[i]; i++)
+{
+for (j = 0; testarr1[j]; j++)
+{
+if (a[i] == testarr1[j])
+{
+a[j] = testarr2;
+break;
+}
+}
+}
+return (a);
+}
