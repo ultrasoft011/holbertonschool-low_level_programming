373e4f8c6935e68e9547742bb77696d256137ff7
ultrasoft011 <55111526+ultrasoft011@users.noreply.github.com>
2019-10-06 12:23:51 -0500

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+* *leet - leet speak
+*@a: Pointer variable
+*Return: 0
+*/
+char *leet(char *a)
+{
+int i = 0;
+int j = 0;
+while (a[i] != '\0')
+{
+
+a y A
+*a[i] = 4;
+
+
+{
+
+
+
+
+
+
+if (src[i] == '\0')
+{
+dest[i] = '\0';
+}
+}
+return (dest);
+}
