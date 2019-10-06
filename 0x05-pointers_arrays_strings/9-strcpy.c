070ab8e5e7ce7fd1ddab333c90cbde48ae09cd08
ultrasoft011 <55111526+ultrasoft011@users.noreply.github.com>
2019-10-05 23:06:21 -0500

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@
 */
 char *_strcpy(char *dest, char *src)
 {
-int i;
+int i, j;
 
 while (src [i]) 
 {
@@ -16,9 +16,9 @@ i++;
 for ( ; src[i] != '\0'; i++) 
 {
 dest[i] = src[i];
-for ( ; i < n; i++)
+for (j = 0 ; j < i; j++)
 {
-dest[i] = '\0';
+dest[j] = '\0';
 }
 }
 return (dest);
