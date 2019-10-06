f2984c2d72d3c9b6b96a223c817402f8100e37c8
ultrasoft011 <55111526+ultrasoft011@users.noreply.github.com>
2019-10-06 13:28:22 -0500

    More11

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,8 +8,19 @@ char *leet(char *a)
 {
 int i = 0;
 int j = 0;
-while (a[i] != '\0')
+
+char testarr1[] = "aeotl";
+char testarr2[] = "AEOTL";
+char numbers[]= "43071";
+
+while (a[i])
 {
-i++;
+for (j = 0; testarr1[j] && testarr2[j]; j++)
+{
+if (a[i] == testarr1[j] || a[i] == testarr2[j])
+{
+a[i] = numbers[j];
 }
+}
+i++;
 }
