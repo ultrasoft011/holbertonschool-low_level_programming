27b9771395994886eab9b066782b84d12d2c0a7e
ultrasoft011 <55111526+ultrasoft011@users.noreply.github.com>
2019-10-06 11:05:29 -0500

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,19 +1,18 @@
 #include "holberton.h"
 /**
-* *_strcpy - copy a string pointed by src to dest 
-*@src: Variable
-*@dest: Variable
+* *_strcpy - copy a string pointed by src to dest
+*@src: Pointer variable
+*@dest: Pointer variable
 *Return: dest
 */
 char *_strcpy(char *dest, char *src)
 {
 int i, j;
-
-while (src [i]) 
+while (src [i] <= '\0') 
 {
 i++;
 }
-for ( ; src[i] != '\0'; i++) 
+for ( ; src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 for (j = 0 ; j < i; j++)
