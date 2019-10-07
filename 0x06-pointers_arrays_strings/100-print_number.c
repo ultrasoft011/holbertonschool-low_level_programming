0cb7192091464623d22965237df99ed4840934e3
ultrasoft011 <55111526+ultrasoft011@users.noreply.github.com>
2019-10-06 21:42:24 -0500

    Create 100-print_number.c

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* *print_number - prints an intenger
+*@n: Variable
+*/
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-' + '0');
+n = -n;
+}
+if (n == 0)
+{
+_putchar('0');
+}
+if (n / 10)
+{
+n = (n / 10);
+_putchar(n % 10 + '0');
+}
+}
