ff094a17f0cf22b895576ab3da5faf8342ec5283
ultrasoft.011 <ultrasoft.011@gmail.com>
2019-10-07 20:19:42 +0000

    Loco

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+**_strchr - locates a string when found a character
+*@s: Pointer to s
+*@c: Variable
+*Return: a pointer or NULL
+*/
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+s = s + 2;
+return (s);
+}
+i++;
+}
+return (s = 0);
+}
