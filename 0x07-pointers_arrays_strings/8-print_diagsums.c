b8e9996b4d47f7534e740addac9c870c133e187f
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-08 22:12:07 +0000

    Loco

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,16 +7,16 @@
 */
 void print_diagsums(int *a, int size)
 {
-int i = 0;
-int j = 0;
-int conta, contb;
-for (i = 0; i < size;  i++)
+int i, j;
+int x = 0;
+int y = 0;
+for (i = 0; i < size; i++)
 {
-conta = conta + a[(size + 1) * i];
+x += a[(size + 1) * i];
 }
 for (j = 0; j < size; j++)
 {
-contb = contb + a[(size - 1) * (j + 1)];
+y += a[(size - 1) * (j + 1)];
 }
-printf("%d, %d\n", conta, contb);
+printf("%d, %d\n", x, y);
 }
