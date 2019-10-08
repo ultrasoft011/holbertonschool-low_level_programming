f6a876afe7d2f33de018ac6895e6feaa883c9570
ultrasoft.011 <ultrasoft.011@gmail.com>
2019-10-08 17:09:41 +0000

    Pointers09

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,23 +1,27 @@
 #include "holberton.h"
 /**
 **_strstr - function that locates a substring
-*@haystack: pointer 
+*@haystack: pointer
 *@needle: pointer
 *Return: a pointer or NULL
 */
 char *_strstr(char *haystack, char *needle)
 {
-for (i = 0; haystack[i] != '\0'; i++)
-{
-for (j = 0; needle[j] != '\0'; j++)
-{
-if (haystack[i] == needle[j])
-{
-haystack = haystack + j;
+int i = 0;
+int j = 0;
+if (*needle == '\0')
 return (haystack);
+for (i = 0; haystack[i]; i++)
+{
+	for (j = 0; needle[j]; j++)
+	{
+		if (haystack[i + j] != needle[j])
+		{
+		break;
+		}
+	}
+if (needle[j] == '\0')
+return (haystack + i);
 }
-}
-s = 0;
-return (s);
-}
+return (0);
 }
