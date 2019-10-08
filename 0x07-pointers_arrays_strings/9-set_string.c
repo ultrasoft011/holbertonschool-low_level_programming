e59bcfa875b3047434cf7ebff4ae1bbc91d415b1
ultrasoft.011 <ultrasoft.011@gmail.com>
2019-10-08 18:24:53 +0000

    Pointer10

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *set_string - sets the value of a pointer to char
+ * @s: Pointer to pointer
+ * @to: Pointer to char
+ * Return: 0
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
