912e76ed574ca390b776bc2088d7f370d8772ac7
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-14 22:06:11 +0000

    4add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,7 +8,8 @@
 */
 int main(int argc, char *argv[]) /* command line Arguments */
 {
-int i, sum = 0;
+int i, j, sum = 0;
+char *x;
 if (argc < 2)
 {
 printf("0\n");
@@ -17,6 +18,15 @@ else
 {
 for (i = 1; i < argc; i++)
 {
+x = argv[i];
+for (j = 0; x[j]; j++)
+{
+if (x[j] < 48 || x[j] > 57)
+{
+printf("Error\n");
+return (1);
+}
+}
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
