d7b3092d855ec5882aa26f7b14b89347d6a11e57
Holberton <holberton@Holbertons-iMac-21.local>
2019-10-15 18:00:29 -0500

    1strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* _strdup - copy of a string given as parameter
+*@str: Pointer to a string
+*Return: the pointer
+*/
+char *_strdup(char *str)
+{
+int count = 0;
+int count2 = 0;
+char *ptr;
+if (str == NULL)
+{
+	return (NULL);
+}
+while (str[count] != '\0')
+{
+	count++;
+}
+ptr = malloc(count * sizeof(char));
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (count2 = 0; count2 < count ; count2++)
+{
+	ptr[count2]= str[count2];
+}
+return (ptr);
+}
