428110555d2805f556e088425ef05740b041ce38
Holberton <holberton@Holbertons-iMac-21.local>
2019-10-15 18:04:44 -0500

    1strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,22 +10,23 @@ char *_strdup(char *str)
 int count = 0;
 int count2 = 0;
 char *ptr;
-if (str == NULL)
-{
-	return (NULL);
-}
-while (str[count] != '\0')
-{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[count] != '\0')
+	{
+		count++;
+	}
 	count++;
-}
-ptr = malloc(count * sizeof(char));
-if (ptr == NULL)
-{
-return (NULL);
-}
-for (count2 = 0; count2 < count ; count2++)
-{
-	ptr[count2]= str[count2];
-}
+	ptr = malloc(count * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (count2 = 0; count2 < count ; count2++)
+	{
+		ptr[count2] = str[count2];
+	}
 return (ptr);
 }
