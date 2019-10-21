a9e6966e134df5580bb51934e8c73bbc73667e2d
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-21 15:55:17 -0500

    django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+
+#ifndef MY_HEADER_H
+#define MY_HEADER_H
+/**
+*struct dog - Django
+*@name: name
+*@age: age
+*@owner: owner
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
