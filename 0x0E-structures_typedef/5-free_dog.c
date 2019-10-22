24d4135d8e93e1397093aa1bb1cabf9e34b25888
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-22 16:39:04 -0500

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *free_dog - free memory
+ *@d: the pointer
+ *Return: void
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+}
