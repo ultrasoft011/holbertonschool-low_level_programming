0671096604bbf3ed67fe87c2f37e216136359245
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-23 14:37:27 -0500

    update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * array_iterator - execute a function
+ * @array: the array
+ * @action: Pointer to a function
+ * @size: the size of the elements
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t count = 0;
+if (array == NULL || action == NULL)
+	return;
+while (size > count)
+{
+	action(array[count]);
+	count++;
+}
+}
