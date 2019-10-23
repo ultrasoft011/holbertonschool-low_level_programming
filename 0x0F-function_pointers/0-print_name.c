5a172bed4f9c4e7621ac8ff1a57e1b002ba7abf8
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-23 14:48:49 -0500

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,12 @@
  * print_name - print name
  * @name: name
  * @f: Pointer to a function
+ * Return:
 */
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
 	return;
+
 f(name);
 }
