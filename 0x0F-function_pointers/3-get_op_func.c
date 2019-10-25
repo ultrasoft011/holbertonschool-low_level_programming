d669b56198d779642b3a4267c4550fbcf7fda5c9
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-25 14:13:06 -0500

    Calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,25 +1,28 @@
 #include "3-calc.h"
-/*
-*get_op_func - selects the correct function to perform the operation asked by the user
+/**
+*get_op_func - selects the correct
 *@s:  is the operator passed as argument to the program
-
-*Return: a pointer to the function that corresponds to the operator given as a parameter
+*Return: a pointer to the function
 */
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-
-for (i = 0; ops[i]; i++)
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+};
+int i = 0;
+/** Loop to get inside on our structure op_t */
+while (i < 5)
 {
-ops[i].op;
+	if (ops[i].op[0] == *s)
+	{
+		return (ops[i].f);
+	}
+i++;
 }
-return (op[i];)
+return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,38 +1,42 @@
 #include "3-calc.h"
 /**
  * main - The calculator
- *
- * Return: Always 0.
+ * @argc: argc the elements
+ * @argv: the content
  */
 int main(int argc, char *argv[])
 {
-// Declaration of the variables
-int a, b;
-char operator;
-int result;
-// get the values and the operator
-operator = argv[2];
-a = atoi([1]);
-b = atoi([3]);  
-// Condition of the number of arguments
-if (argc != 4)
-{
-        printf("Error\n");
-        exit (98);
+	/* Declaration of the variables */
+	int a, b;
+	char *operator;
+	char *poperator;
+	int result;
+	/* Get the values and the operator */
+	poperator = argv[2];
+	operator = argv[2];
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	/* Condition of the number of arguments */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	/* Condition of the operator (+, -, *, / or %) */
+	if ((*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
+	 && *argv[2] != '/' && *argv[2] != '%') || argv[2] == '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	/* Condition if the user tries to divide (/ or %) by 0 */
+	if (*argv[2] == '/' && *argv[2] == '%'  && ((a || b) == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	/* The result of the operation */
+	result = (*get_op_func(operator))(a, b);
+	printf("%d\n", result);
+	return (0);
 }
-// Condition of the operator (+, -, *, / or %), 
-if (argv[2] != "+" || argv[2] != "-" || argv[2] != "*" || argv[2] != "/" || argv[2] != "%")
-{
-        printf("Error\n");
-        exit (99);
-}
-
-// Condition if the user tries to divide (/ or %) by 0 
-if (argv[2] == "/" || argv[2]== "%" ) && (a || b) == 0 
-{
-        printf("Error\n");
-        exit (100);
-}
-// making the operation
-result = get_op_func(char *s))(a, b)
-    return (0);
