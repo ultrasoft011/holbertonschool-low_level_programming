03aa32e77ad51dddfb862e8c4bda4f8c9724b494
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-26 13:16:16 -0500

    Variadic

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef VARIADIC
-#define	VARIADIC
+#ifndef VARIADIC_FUN
+#define	VARIADIC_FUN
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
