da4d9613fe76c0d04826e10cbb7b8c7fe359e4b0
ultrasoft011 <55111526+ultrasoft011@users.noreply.github.com>
2019-10-26 13:24:46 -0500

    Delete variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef VARIADIC_FUN
-#define	VARIADIC_FUN
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-#endif
