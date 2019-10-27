bdb18afc4fcc3de0728b3b9d7ceaf45823865417
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-27 01:12:36 -0500

    updates

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*print_numbers - prints numbers
+*@n: numbers of arguments
+*@separator: the separator
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int counter = 0;
+
+va_list allnumbers;
+va_start(allnumbers, n);
+
+
+if (separator == NULL)
+{
+	separator = "";
+}
+
+for (counter = 0; counter < n; counter++)
+{
+	if (counter != n - 1)
+	{
+		printf("%d%s", va_arg(allnumbers, int), separator);
+	}
+	else
+	{
+		printf("%d", va_arg(allnumbers, int));
+	}	
+
+
+}
+printf("\n");
+va_end(allnumbers);
+}
