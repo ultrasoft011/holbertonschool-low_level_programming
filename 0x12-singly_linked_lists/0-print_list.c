c60430eb2f6d923af6a7eaf6a5bf965aae385147
ultrasoft011 <ultrasoft.011@gmail.com>
2019-10-31 21:23:07 -0500

    0-print

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+* print_list - Write a function that prints all the elements of a list_t list
+* @h: argument
+* Return: the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t ofNodes = 0;
+
+	while (h != NULL)
+	{
+		if (h -> str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h -> str, h -> len);
+		}
+		h = h -> next;
+		ofNodes++;
+	}
+	return (ofNodes);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,20 @@
+#ifndef LISTS
+#define LISTS
+#include <stdlib.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+size_t print_list(const list_t *h);
+} list_t;
+#endif
