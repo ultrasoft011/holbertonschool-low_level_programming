3dcc9bc41d135ce72ee3e8c5f38bb976a2e8715c
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-05 14:26:24 -0500

    list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *last = *head;
 
 	newnode = malloc(sizeof(listint_t));
+
 	if (newnode == NULL)
 	{
 		return (NULL);
@@ -22,12 +23,12 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = newnode;
-		return (NULL);
+		return (*head);
 	}
 	while (last->next != NULL)
 	{
 		last = last->next;
 	}
 last->next = newnode;
-return (newnode);
+return (*head);
 }
