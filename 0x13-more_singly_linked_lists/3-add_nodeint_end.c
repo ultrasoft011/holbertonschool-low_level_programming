0931c03e98b58472296699888babd637d737dbee
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-05 09:41:09 -0500

    linkedlist

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,25 +8,20 @@
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-/* 1. allocate node */
 listint_t *newnode;
 newnode = malloc(sizeof(listint_t));
-/* 2. create the last node (temp) */
 listint_t *last = *head;
-/* 3. put in the data  */
 newnode->n = n;
-/* 4. make next of it as NULL */
 newnode->next = NULL;
-/* 5. if the Linked List is empty, then make the new node as head */
 if (*head == NULL)
 {
-*head = newnode;
-return (NULL);
+	*head = newnode;
+	return (NULL);
 }
-/* 6. Else traverse till the last node */
 while (last->next != NULL)
+{
 	last = last->next;
-/* 7. Change the next of last node */
+}
 last->next = newnode;
 return (newnode);
 }
