b2422ca0b8163b9ae88b0d57fcbcec48c1bfaf2f
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-08 14:31:12 -0500

    bit

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,9 +1,11 @@
 #ifndef HOLBERTON_H
 #define	HOLBERTON_H
+int _pow_recursion(int x, int y);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _putchar(char c);
 #endif
