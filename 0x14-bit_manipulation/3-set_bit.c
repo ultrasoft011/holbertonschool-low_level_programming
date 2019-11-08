b1d8cd5e7320af31216824acb9f6d9992524375d
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-08 14:09:40 -0500

    bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*get_bit - get a bit
+*@n: Variable n
+*@index: Position
+*Return: An integer
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+	{
+		return (-1);
+	}
+	*n = *n | (1 << index);
+	return (1);
+}
