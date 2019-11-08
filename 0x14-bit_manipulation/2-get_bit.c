cb3ae8e0f45b1dbaa473027e7aa395f7227a6390
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-08 13:05:56 -0500

    bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*get_bit - get a bit
+*@n: Variable n
+*@index: Position
+*Return: An integer
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+if ((n >> index) & 1)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+return (n);
+}
