cd58ae2c27d2976655b374a7393c32a9f4ece158
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-08 12:49:58 -0500

    bit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,9 +5,13 @@
 */
 void print_binary(unsigned long int n)
 {
-if (n >= 1)
+if (n > 1)
 {
 	print_binary(n >> 1);
-	printf("%lu", n & 1);
+	putchar ((n & 1) + '0');
+}
+else
+{
+	putchar (n + '0');
 }
 }
