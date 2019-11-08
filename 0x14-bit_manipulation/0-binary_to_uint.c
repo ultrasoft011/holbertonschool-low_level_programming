11ba43acc166685d9ef8285380d116f74f922c35
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-08 14:28:20 -0500

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,6 @@
 #include <stdio.h>
 #include "holberton.h"
-/**
-*_pow_recursion - pow a number
-*@x: Variable x
-*@y: Varaible y
-*Return: if y is lower return -1 means an error
-*/
-int _pow_recursion(int x, int y)
-{
-if (y == 0)
-{
-return (1);
-}
-if (y < 0)
-{
-return (-1);
-}
-else if (y > 0)
-{
-return ((x * _pow_recursion(x, y - 1)));
-}
-else
-{
-return (1 / _pow_recursion(x, -y));
-}
-}
+int _pow_recursion(int x, int y);
 /**
 *binary_to_uint - Write a function converts a binary
 *@b: the pointer to the string
@@ -60,4 +36,29 @@ for (j = 0; b[j]; j++)
 	i--;
 }
 return (sum);
+}
+/**
+*_pow_recursion - pow a number
+*@x: Variable x
+*@y: Varaible y
+*Return: if y is lower return -1 means an error
+*/
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (1);
+}
+if (y < 0)
+{
+return (-1);
+}
+else if (y > 0)
+{
+return ((x * _pow_recursion(x, y - 1)));
+}
+else
+{
+return (1 / _pow_recursion(x, -y));
+}
 }
