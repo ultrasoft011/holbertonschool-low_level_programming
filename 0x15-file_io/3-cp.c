6dbb179edb353465962be512d29a89dbaa1d0bd0
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-13 16:42:21 -0500

    IO

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,9 +13,8 @@
 */
 int main(int argc, char *argv[])
 {
-char *buffer[1024];
+char buffer[1024];
 int file_from, file_to, file_read, file_write;
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -33,18 +32,23 @@ int file_from, file_to, file_read, file_write;
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(99);
 	}
-	while ((file_read = read(file_from, buffer, 1024)))
+	while ((file_read = read(file_from, buffer, 1024) > 0))
 	{
 		file_write = write(file_to, buffer, file_read);
-		if (file_write < file_read)
+		if (file_write != file_read)
 		{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(99);
 		}
 	}
+	if (file_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(file_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
 		exit(100);
 	}
 	if (close(file_from) == -1)
