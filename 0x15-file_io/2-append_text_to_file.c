adcaff558e3f049f319f7b75b0db26eef753ee02
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-12 22:01:36 -0500

    IO

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+*create_file - create a file
+*@filename: filename
+*@text_content: text content
+*Return: create file
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+int ab, cd, count = 0;
+	if (filename == NULL)
+	{
+		return (-1);
+
+	}
+ab = open(filename, O_WRONLY | O_APPEND);
+	if (ab == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		while (text_content[count])
+		{
+			count++;
+		}
+	}
+cd = write(ab, text_content, count);
+
+	if (cd == -1)
+	{
+		return (-1);
+		close(ab);
+	}
+return (1);
+}
