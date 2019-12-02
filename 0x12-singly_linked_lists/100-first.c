a23cbc60aa010b322dc0b33c3d99e7944f2ff4f7
ultrasoft011 <ultrasoft.011@gmail.com>
2019-12-02 17:52:00 -0500

    100first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * startupfun - Entry point
+ *
+ * Return: Always 0
+ */
+void firstChain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
